97f74681a27c7739fa038402870a63eb85392323
samuelescavello <scavellosamuele23@gmail.com>
2024-04-03 15:35:15 +0200

    finish front

--- js/data.js
+++ js/data.js
@@ -167,5 +167,28 @@ export const contacts = [
                 status: 'received'
             }
         ],
+    },
+    {
+        id: 9,
+        name: 'frank',
+        avatar: './img/avatar_2.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                status: 'received'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:51:00',
+                message: 'OK!!',
+                status: 'received'
+            }
+        ],
     }
 ]
