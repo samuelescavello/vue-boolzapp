e8d8d9e1f373828b63b4e0609a8e0d7bcfca74fc
samuelescavello <scavellosamuele23@gmail.com>
2024-04-04 17:23:24 +0200

    milestone 4

--- js/script.js
+++ js/script.js
@@ -1,25 +1,48 @@
 import { contacts } from "./data.js";
 
-const {createApp} = Vue 
+const { createApp } = Vue
 createApp({
-    data(){
-        return{
+    data() {
+        return {
             contacts,
             activeContact: 1,
+            messageText: "",
+            searceText: "",
         }
     },
-    methods:{
-        userActive(id){
+    methods: {
+        userActive(id) {
             this.activeContact = id
         },
+        sendMessage() {
+            const newMessage = {
+                date: new Date().toLocaleString(),
+                message: this.messageText,
+                status: 'sent',
+            }
+            this.activeUser.messages.push(newMessage);
+            this.messageText = '';
+            setTimeout(()=>{
+                const newMessage = {
+                    date: new Date().toLocaleString(),
+                    message: 'ok',
+                    status: 'received'
+                }
+                this.activeUser.messages.push(newMessage);
+            }, 1000);
+        }
     },
-    computed:{
-        activeUser(){
-           return this.contacts.find((el)=>el.id === this.activeContact)
-          
+    computed: {
+        activeUser() {
+            return this.contacts.find((el) => el.id === this.activeContact)
+
         },
+        filteredContact(){
+            return this.contacts.filter((el)=> el.name.toLowerCase().includes
+            (this.searceText.toLowerCase()));
+        }
     },
-    mounted(){
+    mounted() {
         console.log(this.contacts)
     },
 
