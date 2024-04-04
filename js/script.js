d779e3ab0704e3a774f755beb420ccaf1df803c5
samuelescavello <scavellosamuele23@gmail.com>
2024-04-04 14:34:50 +0200

    activebg

--- js/script.js
+++ js/script.js
@@ -5,12 +5,18 @@ createApp({
     data(){
         return{
             contacts,
+            activeContact: 1,
         }
     },
     methods:{
-
+        userActive(id){
+            this.activeContact = id
+        },
     },
+    
     mounted(){
         console.log(this.contacts)
-    }
+    },
+
+
 }).mount('#app')
